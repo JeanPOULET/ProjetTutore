94d9944e7a2841bfe35e7653e4d75df77f8ecae6
JeanPOULET <mathieu.bart3@gmail.com>
2019-11-25 13:54:27 +0100

    modif nom

--- code/game.cc
+++ code/game.cc
@@ -4,32 +4,33 @@
 #include <gf/RenderWindow.h>
 #include <gf/Window.h>
 int main() {
-  // initialization
-  gf::Window window("My great game", { 1024, 768 });
-  gf::RenderWindow renderer(window);
-  // game loop
-  gf::Clock clock;
-  renderer.clear(gf::Color::White);
-  while (window.isOpen()) {
-    // 1. input
-    gf::Event event;
-    while (window.pollEvent(event)) {
-      switch (event.type) {
-        case gf::EventType::Closed:
-          window.close();
-          break;
-      case gf::EventType::KeyPressed:
-        break;
-      default:
-        break;
-      }
-    }
-    // 2. update
-    float dt = clock.restart().asSeconds();
-    // 3. draw
-    renderer.clear();
-    // draw everything
-    renderer.display();
-  }
-  return 0;
+	static constexpr gf::Vector2u ScreenSize(1024, 768);
+	// initialization
+	gf::Window window("K.G.B.", ScreenSize);
+	gf::RenderWindow renderer(window);
+	// game loop
+	gf::Clock clock;
+	renderer.clear(gf::Color::White);
+	while (window.isOpen()) {
+		// 1. input
+		gf::Event event;
+		while (window.pollEvent(event)) {
+			switch (event.type) {
+				case gf::EventType::Closed:
+				window.close();
+				break;
+			case gf::EventType::KeyPressed:
+				break;
+			default:
+				break;
+			}
+		}
+		// 2. update
+		float dt = clock.restart().asSeconds();
+		// 3. draw
+		renderer.clear();
+		// draw everything
+		renderer.display();
+		}
+	return 0;
 }
