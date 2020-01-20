97021c35790e3e69b835fd7ca0051d3759692fe9
JeanPOULET <mathieu.bart3@gmail.com>
2020-01-21 00:30:23 +0100

    map

---
+++ data/KGB/map/ressources/kenney_medievalrtspack/Tilesheet/medieval_tilesheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="medieval_tilesheet" tilewidth="64" tileheight="64" spacing="32" margin="32" tilecount="126" columns="18">
+ <image source="medieval_tilesheet.png" width="1760" height="704"/>
+</tileset>
