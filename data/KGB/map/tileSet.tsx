f9da01ad4dad893282af99a8ddc08d3503c75a72
JeanPOULET <mathieu.bart3@gmail.com>
2020-01-21 20:50:07 +0100

    map V1

---
+++ data/KGB/map/tileSet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="BaseChip_pipo" tilewidth="32" tileheight="32" tilecount="1064" columns="8">
+ <image source="Pipoya_RPG/[Base]BaseChip_pipo.png" width="256" height="4256"/>
+</tileset>
